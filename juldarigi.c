d465c2eb4d839e05e099b8d8ce5bf791ff4194e3
hyeog62 <alsgur062231#naver.com>
2023-12-07 20:04:01 +0900

    줄다리기 힘 계산완

--- juldarigi.c
+++ juldarigi.c
@@ -10,8 +10,35 @@
 
 void juldarigi(void);
 void juldarigi_init(void);
+//void juldarigi_tick(void);
+void juldarigi_move(void);
 
 int px[PLAYER_MAX], py[PLAYER_MAX], period[PLAYER_MAX];  // 각 플레이어 위치, 이동 주기
+float juldarigi_pow;
+
+void juldarigi_move(void) {
+	int p = player;  // 이름이 길어서...
+	if (juldarigi_pow <= 0) { // 왼쪽힘이 더 강함
+		//back_buf에 저장
+		for (int i = 0; i < n_player; i++) {
+			//back_buf[px[1]][py[i]] = back_buf[px[1]][py[i]];//이동할 위치
+			//back_buf[px[1]][py[i]] = back_buf[px[1]][py[i]-1];//이동전 위치
+		}
+	}
+	else{                   // 오른쪽힘이 더 강함
+		//back_buf에 저장
+		for (int i = 0; i < n_player; i++) {
+			//back_buf[px[1]][py[i]] = back_buf[px[1]][py[i]];//이동할 위치
+			//back_buf[px[1]][py[i]] = back_buf[px[1]][py[i]+1];//이동전 위치
+		}
+	}
+}
+
+void juldarigi_tick(void) {
+	if (tick % 1000 == 0) {   //1초체킹
+		juldarigi_move();
+	}
+}
 
 void juldarigi_init(void) {
 	map_init(3, 27);
@@ -37,8 +64,29 @@ void juldarigi_init(void) {
 		}
 		back_buf[px[i]][py[i]] = '0' + i;  // (0 .. n_player-1)
 	}
+	tick = 0;
 }
+void players_pow(void);
+float odd_pow = 0;
+float even_pow = 0;
 
+void players_pow(void) {
+	for (int i = 0; i < n_player; i++) {
+		if (i % 2 == 0) {
+			if (player[i].hasitem == true) {
+				even_pow += player[i].item.str_buf;
+			}
+			even_pow += player[i].str;
+		}
+		else{
+			if (player[i].hasitem == true) {
+				odd_pow += player[i].item.str_buf;
+			}
+			odd_pow += player[i].str;
+		}
+	}
+	juldarigi_pow = even_pow - odd_pow;
+}
 
 void juldarigi(void) {
 	juldarigi_init();
@@ -50,16 +98,18 @@ void juldarigi(void) {
 	printf(" ");
 	gotoxy(2, 14);
 	printf(" ");
+	players_pow();
 
 	while (1) {
 
+		key_t key = get_key();
+		if (key == K_QUIT) {
+			break;
+		}
+		juldarigi_tick();
 		display();
 		gotoxy(5, 0);
-		printf("str: %4d",0);
-		Sleep(10);
-			//if (tick % 3000 == 0) {
-			//	dialog("초후 게임이 시작됩니다");
-			//	break;
-			//}
+		printf("str: %5.1f", juldarigi_pow);
+		tick += 10;
 	}
 }
